855759e1c5ce2d022054e1f0cd8ca8ec1beabf8f
Rodrigo Bezerra <rodbn87@gmail.com>
2023-11-19 16:13:09 +0000

    Ajustando os menus vertical e horizontal

--- loja/vaitudoabaixo/static/js/script.js
+++ loja/vaitudoabaixo/static/js/script.js
@@ -1,12 +1,20 @@
 function toggleMenu(event) {
+    console.log("Função toggleMenu chamada");
     var menu = document.getElementById("menu");
-    var logo = document.getElementById("logo");
-
+    var logo = document.querySelector(".logo-header");
     if (event.type === "mouseover") {
         menu.style.display = "block";
-        logo.src = "{% static 'images/VaiTudoAbaixo-LOGO-NOTEXT.png' %}";
+        logo.src = "/static/images/VaiTudoAbaixo-LOGO-WTEXT.png";
+        logo.style.width = '150px';
+        logo.style.marginTop = "20px";
+        logo.style.marginBottom = "20px";
     } else if (event.type === "mouseout") {
         menu.style.display = "none";
-        logo.src = "{% static 'images/VaiTudoAbaixo-LOGO2.png' %}";
+        logo.src = "/static/images/VaiTudoAbaixo-LOGO-white.png";
+        logo.style.width = '60%';
+        logo.style.marginTop = "0";
+        logo.style.marginBottom = "0";
     }
 }
+
+
